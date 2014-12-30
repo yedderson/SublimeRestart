88c641f9d4f9f3a2cca8d902bff6a8d5c5065ca7
Hassen <yedderson@users.noreply.github.com>
2014-12-30 22:42:04 +0100

    Added support for Sublime text 3
    This should be working now on Windows, Mac and Linux

--- restart.py
+++ restart.py
@@ -1,17 +1,29 @@
-import sublime, sublime_plugin, os, sys
+import sublime, sublime_plugin, os, sys, subprocess
 
 __author__ = 'Hassen Ben Yeddder'
 __email__ = 'hassenbenyedder@gmail.com'
 
 class RestartCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if sys.platform == 'win32':
-            os.execl(sys.executable,' ')
-        else:
-            path = sublime.load_settings("Restart.sublime-settings").get("path")
-            if path:
-                os.execl(path)
-            elif sys.platform == 'darwin':
+        path = sublime.load_settings("Restart.sublime-settings").get("path")
+        
+        if path:
+            os.execl(path)
+        elif sys.platform == 'win32':
+            if sublime.version()[:1]=='3':
+                subprocess.call('taskkill /im sublime_text.exe /f & cmd /C "C:\\Program Files\\Sublime Text 3\\sublime_text.exe"', shell=True)
+            else:
+                os.execl(sys.executable,' ')
+        elif sys.platform == 'darwin':
+            #Restarting ST3 on mac
+            if sublime.version()[:1]=='3':
+                subprocess.call("pkill subl && /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl", shell=True)
+            else:
                 os.execl('/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl')
+        else:
+            #Restarting ST3 on linux
+            if sublime.version()[:1]=='3':
+                subprocess.call("pkill 'sublime_text' && /opt/sublime_text/sublime_text", shell=True)
+                
             else:
                 os.execl('/usr/local/sublime-text/sublime_text')
